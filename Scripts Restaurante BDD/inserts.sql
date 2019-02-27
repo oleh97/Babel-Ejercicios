b73af9a7432373d604c70906d23bc5d60c86d9eb
david23garcia <david.garciagarcia@estudiante.uam.es>
2019-02-27 10:50:47 +0100

    inset platos

--- Scripts Restaurante BDD/inserts.sql
+++ Scripts Restaurante BDD/inserts.sql
@@ -1,7 +1,7 @@
-INSERT INTO categorias(tipo) VALUES
+INSERT INTO categorias (tipo) VALUES
 ("Bebida"),("Entrantes"),("Plato Principal"),("Primer Plato"),("Segundo Plato"),("Postre");
 
-INSERT INTO rol (tipo) VALUES
+INSERT INTO roles (tipo) VALUES
 ("Cliente"), ("Cliente Vip"), ("Camarero Barra"),
 ("Camarero Mesas"), ("Cocinero"), ("Jefe Cocina"), 
 ("Ayudante Cocina"), ("Gerente");
@@ -9,3 +9,23 @@ INSERT INTO rol (tipo) VALUES
 INSERT INTO estados (tipo) VALUES
 ("Pedido"), ("Cocinando"), ("Cocinado"), ("En Reparto"),
 ("En Preparacion"), ("Preparado");
+
+INSERT INTO platos (categoria, nombre) VALUES
+(1, "Coca-Cola"),
+(2, "Fingers Pollo"),
+(3, "Haburguesa de Pollo"),
+(4, "Tarta de Chocolate"),
+(5, "Ensalada"),
+(6, "Hamburguesa Queso"),
+(1, "Cerveza"),
+(2, "Patatas Bravas"),
+(3, "Haburguesa de la Casa"),
+(4, "Bola de Helado"),
+(5, "Espaguetis Boloñesa"),
+(6, "Lomo con Patatas"),
+(1, "Tinto Verano"),
+(2, "Palomitas de Queso"),
+(3, "Costillar 1/2"),
+(4, "Tiramisu"),
+(5, "Sopa"),
+(6, "Sandwich Especial");
