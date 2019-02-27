8ec4e29439e90620b3242fa89de4eea56ad52d68
david23garcia <david.garciagarcia@estudiante.uam.es>
2019-02-27 10:41:51 +0100

    inserts rol y estados

--- Scripts Restaurante BDD/inserts.sql
+++ Scripts Restaurante BDD/inserts.sql
@@ -1,2 +1,11 @@
 INSERT INTO categorias(tipo) VALUES
 ("Bebida"),("Entrantes"),("Plato Principal"),("Primer Plato"),("Segundo Plato"),("Postre");
+
+INSERT INTO rol (tipo) VALUES
+("Cliente"), ("Cliente Vip"), ("Camarero Barra"),
+("Camarero Mesas"), ("Cocinero"), ("Jefe Cocina"), 
+("Ayudante Cocina"), ("Gerente");
+
+INSERT INTO estados (tipo) VALUES
+("Pedido"), ("Cocinando"), ("Cocinado"), ("En Reparto"),
+("En Preparacion"), ("Preparado");
