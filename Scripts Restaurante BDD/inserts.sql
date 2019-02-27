6bb72344fbda5d3c20909ae9ab8f0f3bef259a99
david23garcia <david.garciagarcia@estudiante.uam.es>
2019-02-27 11:41:38 +0100

    arreglado insert usuarios

--- Scripts Restaurante BDD/inserts.sql
+++ Scripts Restaurante BDD/inserts.sql
@@ -1,17 +1,22 @@
+
+DELETE FROM categorias;
 INSERT INTO categorias (tipo) VALUES
 ("Bebida"),("Entrantes"),("Plato Principal"),("Primer Plato"),("Segundo Plato"),("Postre");
 
+DELETE FROM roles;
 INSERT INTO roles (tipo) VALUES
 ("Cliente"), ("Cliente Vip"), ("Camarero Barra"),
 ("Camarero Mesas"), ("Cocinero"), ("Jefe Cocina"), 
 ("Ayudante Cocina"), ("Gerente");
 
+DELETE FROM estados;
 INSERT INTO estados (tipo) VALUES
 ("Pedido"), ("Cocinando"), ("Cocinado"), ("En Reparto"),
 ("En Preparacion"), ("Preparado");
 
-INSERT INTO usuarios (rol, usuario, password) VALUES
-(8,"Paco","pacogerente"), -- Gerente ID = 8
+DELETE FROM usuarios;
+INSERT INTO usuarios (rol, usuario, pass) VALUES
+(8, "Paco", "pacogerente"), -- Gerente ID = 8
 (6, "Ramon", "ramonJefeCocina"), -- Jefe de Cocina ID = 6
 (7, "Julian", "julianBecario"), -- Ayudante de Cocina ID = 7
 (7, "David", "davidBecario"), -- Ayudante de Cocina ID = 7
@@ -26,18 +31,19 @@ INSERT INTO usuarios (rol, usuario, password) VALUES
 (3, "Laura", "lauraBarra"), -- Camarero Barra ID = 3
 (3, "Monica", "monicaBarra"), -- Camarero Barra ID = 3
 (3, "Ignacio", "ignacioBarra"), -- Camarero Barra ID = 3
-(1, "cliente1" "1234"), -- Cliente ID = 1
-(1, "cliente2" "1234"), -- Cliente ID = 1
-(1, "cliente3" "1234"), -- Cliente ID = 1
-(1, "cliente4" "1234"), -- Cliente ID = 1
-(1, "cliente5" "1234"), -- Cliente ID = 1
-(1, "cliente6" "1234"), -- Cliente ID = 1
-(1, "cliente7" "1234"), -- Cliente ID = 1
-(1, "cliente8" "1234"), -- Cliente ID = 1
-(2, "clienteV1" "1234"), -- Cliente VIP ID = 2
-(2, "clienteV2" "1234"), -- Cliente VIP ID = 2
-(2, "clienteV3" "1234"); -- Cliente VIP ID = 2
+(1, "cliente1", "1234"), -- Cliente ID = 1
+(1, "cliente2", "1234"), -- Cliente ID = 1
+(1, "cliente3", "1234"), -- Cliente ID = 1
+(1, "cliente4", "1234"), -- Cliente ID = 1
+(1, "cliente5", "1234"), -- Cliente ID = 1
+(1, "cliente6", "1234"), -- Cliente ID = 1
+(1, "cliente7", "1234"), -- Cliente ID = 1
+(1, "cliente8", "1234"), -- Cliente ID = 1
+(2, "clienteV1", "1234"), -- Cliente VIP ID = 2
+(2, "clienteV2", "1234"), -- Cliente VIP ID = 2
+(2, "clienteV3", "1234"); -- Cliente VIP ID = 2
 
+DELETE FROM platos;
 INSERT INTO platos (categoria, nombre) VALUES
 (1, "Coca-Cola"),
 (2, "Fingers Pollo"),
