990e0e871360a41856430163b600f29e3b7e160f
oleh97 <oleh.hrinchenko@gmail.com>
2019-03-05 12:57:24 +0100

    finito

--- AJAX Ingresos-gastos/app.js
+++ AJAX Ingresos-gastos/app.js
@@ -8,37 +8,53 @@ function getIngGas() {
         return resp.json();
     })
     .then(ingresosGastos => {
+        var ingresosTotales = 0;
+        var gastosTotales = 0;
         let ul = document.getElementById("lista");
         let arr = [];
         for(let id in ingresosGastos) {
-            arr.push(ingresosGastos[id])
+           if(ingresosGastos[id].qty != undefined){
+                arr.push(ingresosGastos[id])
+                if(ingresosGastos[id].qty < 0) gastosTotales -= parseInt(ingresosGastos[id].qty)
+                else if(ingresosGastos[id].qty > 0) {
+                    ingresosTotales += parseInt(ingresosGastos[id].qty)
+                }
+           }
         }
-        ul.innerHTML = arr.map(elem => `<li>Concepto :${elem.concept} /// Cantidad: ${elem.qty}</li>`).join("");
+        ul.innerHTML = arr.map(elem =>  `<li style="background-color: ${elem.qty < 0 ? "red" : "green"}">Concepto :${elem.concept}
+                                         /// Cantidad: ${elem.qty}</li>`).join("");
+        let aGastos = document.getElementById("gastosTotales");
+        let aIngresos = document.getElementById("ingresosTotales");
+        let aTotal = document.getElementById("total");
+        aGastos.textContent = " "+gastosTotales;
+        aIngresos.textContent = " "+ingresosTotales;
+        aTotal = " " + (parseInt( ingresosTotales) - parseInt( gastosTotales));
     })
 }
 
 getIngGas();
+//nuke();
 
-btnGasto.addEventListener("click", (event) => {
-    let inputConcepto = document.getElementById("conceptoGasto");
-    let concepto = inputConcepto.value;
-
-    let inputCantidad = document.getElementById("cantidadGasto");
-    let cantidad = inputCantidad.value;
-    fetch(URL, {
-        method: "POST",
-        body: JSON.stringify({concept: concepto , qty: cantidad})
+function nuke() {
+    fetch(URL)
+    .then(resp => {
+        return resp.json();
     })
-    getIngGas();
-    inputConcepto.value = "";
-    inputCantidad.value = "";
-})
+    .then(ingresosGastos => {
+        for(let id in ingresosGastos) {
+            fetch(URL, {
+                method: "DELETE",
+                body: JSON.stringify(ingresosGastos[id])
+            })
+        }
+    })
+}
 
-btnIngreso.addEventListener("click", (event) => {
-    let inputConcepto = document.getElementById("conceptoIngreso");
+btn.addEventListener("click", (event) => {
+    let inputConcepto = document.getElementById("concepto");
     let concepto = inputConcepto.value;
 
-    let inputCantidad = document.getElementById("cantidadIngreso");
+    let inputCantidad = document.getElementById("cantidad");
     let cantidad = inputCantidad.value;
     fetch(URL, {
         method: "POST",
