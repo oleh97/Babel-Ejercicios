d640f8d2172bc9f959c7e8c94887ce41692fb513
oleh97 <oleh.hrinchenko@gmail.com>
2019-03-13 12:50:53 +0100

    Formulario CV Angular

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -2,12 +2,17 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { CmpEjercicioPipeComponent } from './cmp-ejercicio-pipe/cmp-ejercicio-pipe.component';
+import { OcultoPipe } from './cmp-ejercicio-pipe/oculto.pipe';
 import { CmpCurriculumComponent } from './cmp-curriculum/cmp-curriculum.component';
 import { CmpEjercicioPipeComponent } from './cmp-ejercicio-pipe/cmp-ejercicio-pipe.component';
 import { OcultoPipe } from './cmp-ejercicio-pipe/oculto.pipe';
 
 @NgModule({
   declarations: [
+    AppComponent,
+    CmpEjercicioPipeComponent,
+    OcultoPipe,
     AppComponent,
     CmpCurriculumComponent,
     AppComponent,
---
+++ Angular/src/app/cmp-curriculum/cmp-curriculum.component.ts
@@ -0,0 +1,46 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-cmp-curriculum',
+  templateUrl: './cmp-curriculum.component.html',
+  styleUrls: ['./cmp-curriculum.component.css']
+})
+export class CmpCurriculumComponent implements OnInit {
+  nombre = 'NOMBRE';
+  apellidos = 'APELLIDOS';
+  fecha: Date;
+  email = '';
+  foto = '';
+  skills = [];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  setEmail(e: string) {
+    this.email = e;
+  }
+
+  setNombre(n: string) {
+    this.nombre = n;
+  }
+
+  setApellidos(a: string) {
+    this.apellidos = a;
+  }
+
+  setFecha(d: Date) {
+    this.fecha = d;
+  }
+
+  setFoto(f: string) {
+    console.log(f);
+    this.foto = f;
+  }
+
+  addSkill(s: string) {
+    this.skills.push(s);
+    document.getElementById('inputSkill').value = '';
+  }
+
+}
