ebf7e96cae50c0f2202c4a0643f6bd80e856293d
oleh97 <oleh.hrinchenko@gmail.com>
2019-03-13 09:13:16 +0100

    cambios

--- Ejercicio DOM 6/app.js
+++ Ejercicio DOM 6/app.js
@@ -24,50 +24,23 @@ imgLeft.src = imgs[indice-1].src;
 imgCenter.src = imgs[indice].src;
 imgRight.src = imgs[indice+1].src;
 
-for(let i of btns) {
-    i.addEventListener("click", event => {
-        if(i.id == "btnNext") {
-            indice = indice +1;
-            if(indice == imgs.length) {
-                indice = 0;
-            }
+imgRight.addEventListener("click", event =>{
+    indice++;
+    if(indice == imgs.length) indice = 0;
+    transicionImagenes(indice);
+})
 
-            imgCenter.src = imgs[indice].src;
+imgLeft.addEventListener("click", envent => {
+    indice--;
+    if(indice == -1) indice = imgs.length-1;
+    transicionImagenes(indice);
+})
+
+function transicionImagenes(indice){
+    imgCenter.src = imgs[indice].src;
             if(indice == imgs.length-1) imgRight.src = imgs[0].src; 
             else imgRight.src = imgs[indice+1].src;
             if(indice == 0) imgLeft.src = imgs[imgs.length-1].src
             else imgLeft.src = imgs[indice-1].src;
-        }
-        else if(i.id == "btnBack"){
-            indice--;
-            if(indice == -1) {
-                indice = imgs.length-1;
-            }
-            
-            imgCenter.src = imgs[indice].src;
-            if(indice == imgs.length-1) imgRight.src = imgs[0].src; 
-            else imgRight.src = imgs[indice+1].src;
-            if(indice == 0) imgLeft.src = imgs[imgs.length-1].src
-            else imgLeft.src = imgs[indice-1].src;
-        }
-    })
 }
 
-function cambiarIndice(dir) {
-    if(dir == "izq") {
-        if(indice == 0) {
-            indice = imgs.length-1;
-        }
-        else {
-            indice--
-        }
-    }
-    if(dir == "der") {
-        if(indice == imgs.length-1) {
-            indice = 0;
-        }
-        else {
-            indice++
-        }
-    }
-}
