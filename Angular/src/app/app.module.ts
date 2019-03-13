4961ca9c6af6592698ea92bc21a5c146b28153d1
david23garcia <david.garciagarcia@estudiante.uam.es>
2019-03-13 12:53:52 +0100

    a ver si conseguimos vincular todo bien :)

--- Angular/src/app/app.module.ts
+++ Angular/src/app/app.module.ts
@@ -5,19 +5,14 @@ import { AppComponent } from './app.component';
 import { CmpEjercicioPipeComponent } from './cmp-ejercicio-pipe/cmp-ejercicio-pipe.component';
 import { OcultoPipe } from './cmp-ejercicio-pipe/oculto.pipe';
 import { CmpCurriculumComponent } from './cmp-curriculum/cmp-curriculum.component';
-import { CmpEjercicioPipeComponent } from './cmp-ejercicio-pipe/cmp-ejercicio-pipe.component';
-import { OcultoPipe } from './cmp-ejercicio-pipe/oculto.pipe';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     CmpEjercicioPipeComponent,
     OcultoPipe,
-    AppComponent,
     CmpCurriculumComponent,
-    AppComponent,
-    CmpEjercicioPipeComponent,
-    OcultoPipe
   ],
   imports: [
     BrowserModule
